87a8aa7a3125dc1b1720c106d0d5d5222675ef9a
Kelsey Hughes <kelbri10@gmail.com>
2023-04-14 15:41:24 -0400

    added js

---
+++ app.js
@@ -0,0 +1,30 @@
+const submitRating = () => { 
+    const ratingsArr = document.getElementsByName('rating'); 
+
+    ratingsArr.forEach(rating => { 
+        if(rating.checked){ 
+            let userChoice = rating.dataset.number; 
+
+            thankYou(userChoice); 
+        }; 
+    }); 
+}
+
+const thankYou = (userChoice) => { 
+    //You selected Add rating here out of 5
+
+    //Thank you
+  
+    //We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!
+  
+    const wrapper = document.querySelector('.wrapper'); 
+
+    wrapper.innerHTML = 
+    `<img src='images/illustration-thank-you.svg' />
+    <p>You selected ${userChoice} out of 5</p>
+    <div class='thankyou_container'>
+        <h1>Thank you!</h1>
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, dont hesitate to get in touch!</p>
+    </div>`; 
+    
+}
